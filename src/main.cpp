670b394bb1cb8151e52fe085d2e9b060d7165ede
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-05-10 17:38:08 +0000

    push

--- src/main.cpp
+++ src/main.cpp
@@ -33,66 +33,68 @@ int main(void)
         auto now = std::chrono::steady_clock::now();
         double dt = std::chrono::duration_cast<std::chrono::milliseconds>(now - past).count() / 1000.0f;
         past = now;
-        m.rotate(90 * dt, 90 * dt, 90 * dt);
+        m.rotate(0 * dt, 90 * dt, 0 * dt);
         sr::draw(m, c);
         sr::draw(m1, c);
         sr::draw(m2, c);
         td_drawPoint(width/2, height/2, '+', TD_COLOR_MAGENTA, TD_COLOR_DEFAULT);
         sr::display();
 
+        double speed = 15;
+
         if (ch == 'w')
         {
             m3::vec4 mod = m3::vec4::normalized(c.rotation * c.front * m3::quat::inverse(c.rotation));
-            mod.data[0] *= 30 * dt;
-            mod.data[1] *= 30 * dt;
-            mod.data[2] *= 30 * dt;
+            mod.data[0] *= speed * dt;
+            mod.data[1] *= speed * dt;
+            mod.data[2] *= speed * dt;
 
             c.pos = c.pos + mod;
         }
         if (ch == 's')
         {
             m3::vec4 mod = m3::vec4::normalized(c.rotation * c.front * m3::quat::inverse(c.rotation));
-            mod.data[0] *= 30 * dt;
-            mod.data[1] *= 30 * dt;
-            mod.data[2] *= 30 * dt;
+            mod.data[0] *= speed * dt;
+            mod.data[1] *= speed * dt;
+            mod.data[2] *= speed * dt;
 
             c.pos = c.pos - mod;
         }
         if(ch == 'd')
         {
             m3::vec4 mod = m3::vec4::normalized(c.rotation * m3::vec4::normalized(c.front % c.world_up)  * m3::quat::inverse(c.rotation));
-            mod.data[0] *= 30 * dt;
-            mod.data[1] *= 30 * dt;
-            mod.data[2] *= 30 * dt;
+            mod.data[0] *= speed * dt;
+            mod.data[1] *= speed * dt;
+            mod.data[2] *= speed * dt;
 
             c.pos = c.pos - mod;
         }
         if(ch == 'a')
         {
             m3::vec4 mod = m3::vec4::normalized(c.rotation * m3::vec4::normalized(c.front % c.world_up)  * m3::quat::inverse(c.rotation));
-            mod.data[0] *= 30 * dt;
-            mod.data[1] *= 30 * dt;
-            mod.data[2] *= 30 * dt;
+            mod.data[0] *= speed * dt;
+            mod.data[1] *= speed * dt;
+            mod.data[2] *= speed * dt;
 
             c.pos = c.pos + mod;
         }
         if(ch == ' ')
         {
-            c.pos.data[1] += 30 * dt;
+            c.pos.data[1] += speed * dt;
         }
         if(ch == 'v')
         {
-            c.pos.data[1] -= 30 * dt;
+            c.pos.data[1] -= speed * dt;
         }
         if(ch == 'q')
         {
-            m3::quat rot(-360 * dt, m3::vec4(0, 1, 0, 0));
+            m3::quat rot(-180 * dt, m3::vec4(0, 1, 0, 0));
 
             c.rotation = c.rotation * rot;
         }
         if(ch == 'e')
         {
-            m3::quat rot(360 * dt, m3::vec4(0, 1, 0, 0));
+            m3::quat rot(180 * dt, m3::vec4(0, 1, 0, 0));
 
             c.rotation = c.rotation * rot;
         }
